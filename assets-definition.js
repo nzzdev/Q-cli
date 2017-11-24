a8dfe1ed8e7d5cbcce7c5190fef71b1366ff22a5
Sharon Funke <sharon.funke@nzz.ch>
2017-11-24 18:17:18 +0100

    Intitial commit with index.html and asset config

---
+++ assets-definition.js
@@ -0,0 +1,18 @@
+let assets = {
+  sophie : [
+    'sophie-q@^1',
+    'sophie-font@^1',
+    'sophie-color@^1',
+    'sophie-viz-color@^1',
+    'sophie-input@^1',
+    //'sophie-nzzas-q@^1', 
+    //'sophie-nzzas-font@^1',
+    //'sophie-nzzas-color@^1', 
+    //'sophie-nzzas-viz-color@^1',
+    //'sophie-nzzas-input@^1'
+  ],
+  context: [
+    'https://context-service.st.nzz.ch/stylesheet/all/nzz.ch.css',
+    //'https://context-service.st.nzz.ch/stylesheet/all/nzzas.nzz.ch.css'
+  ]
+}
