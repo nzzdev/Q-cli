7d82fcce603b6b9cffba65514a2923e814cba59e
Sharon Funke <sharon.funke@nzz.ch>
2018-01-08 14:32:47 +0100

    Add server skeleton, add command to bootstrap server, add inert as dependency in tool

---
+++ server-skeleton/hapi/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: process.env.COUCH_HOST,
+  database: process.env.COUCH_DB,
+  user: process.env.COUCH_USER,
+  pass: process.env.COUCH_PASS 
+};
---
+++ server-skeleton/hapi/config/targets.js
@@ -0,0 +1,32 @@
+const Confidence = require('confidence');
+
+const targets = [
+  {
+    key: 'target1',
+    label: 'Target 1', 
+    type: 'web',
+    context: {
+      stylesheets: [
+        {
+          url: 'https://example.com/stylesheet-for-preview.css'
+        }
+      ],
+      background: {
+        color: 'white'
+      }
+    }
+  }
+]
+
+const env = process.env.APP_ENV || 'local';
+const store = new Confidence.Store(targets);
+
+module.exports.get = (key, criteria = {}) => {
+  criteria = Object.assign({ env: env }, criteria)
+  return store.get(key, criteria)
+}
+
+module.exports.meta = (key, criteria = {}) => {
+  criteria = Object.assign({ env: env }, criteria)
+  return store.meta(key, criteria)
+}
