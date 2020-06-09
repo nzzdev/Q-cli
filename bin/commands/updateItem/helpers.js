f60b2e1035118354901e12e6c0dcba3ed345e23c
Manuel Roth <manuelroth@hotmail.ch>
2020-06-09 16:58:49 +0200

    Support updating q items from different environments

--- bin/commands/updateItem/helpers.js
+++ bin/commands/updateItem/helpers.js
@@ -1,10 +1,141 @@
 const fetch = require("node-fetch");
+const deepmerge = require("deepmerge");
 const path = require("path");
 const Ajv = require("ajv");
 const ajv = new Ajv();
+const promptly = require("promptly");
+const Configstore = require("configstore");
+const package = require("../../../package.json");
+const config = new Configstore(package.name, {});
 
-async function getQServerAccessToken(QServerBaseUrl, username, password) {
-  const response = await fetch(`${QServerBaseUrl}authenticate`, {
+async function updateItem(item, config) {
+  const qServer = config.get(`${item.metadata.environment}.qServer`);
+  const accessToken = config.get(`${item.metadata.environment}.accessToken`);
+  const existingItem = await getItem(qServer, accessToken, item.metadata.id);
+  const newItem = deepmerge(existingItem, item.item, {
+    arrayMerge: (destArr, srcArr) => srcArr,
+  });
+
+  await saveItem(qServer, accessToken, newItem);
+  console.log(
+    `Successfully updated item with id ${item.metadata.id} on ${item.metadata.environment} environment`
+  );
+}
+
+async function getItem(qServer, accessToken, id) {
+  const response = await fetch(`${qServer}item/${id}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  if (response.ok) {
+    return await response.json();
+  } else {
+    logUnexpectedError();
+  }
+}
+
+async function saveItem(qServer, accessToken, item) {
+  delete item.updatedDate;
+  const response = await fetch(`${qServer}item`, {
+    method: "PUT",
+    body: JSON.stringify(item),
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    return await response.json();
+  } else {
+    logUnexpectedError();
+  }
+}
+
+function logUnexpectedError(error) {
+  console.log(
+    "An unexpected error occured. Please check the entered information and try again."
+  );
+  console.log(JSON.stringify(error));
+}
+
+function getCurrentDirectoryBase() {
+  return path.basename(process.cwd());
+}
+
+function validateConfig(config) {
+  const isValid = ajv.validate(require("./schema.json"), config);
+  return {
+    isValid: isValid,
+    errorsText: ajv.errorsText(),
+  };
+}
+
+function getEnvironments(qConfig) {
+  const environments = new Set();
+  for (const item of qConfig.items) {
+    environments.add(item.metadata.environment);
+  }
+
+  return Array.from(environments);
+}
+
+async function setupConfig(qConfig, clearConfig) {
+  if (clearConfig) {
+    config.clear();
+  }
+  const environments = getEnvironments(qConfig);
+  for (const environment of environments) {
+    if (!config.get(`${environment}.qServer`)) {
+      const qServer = new URL(
+        await promptly.prompt(
+          `Enter the Q-Server url for ${environment} environment: `
+        )
+      ).toString();
+      config.set(`${environment}.qServer`, qServer);
+    }
+
+    if (!config.get(`${environment}.accessToken`)) {
+      const qServer = config.get(`${environment}.qServer`);
+      const username = await promptly.prompt("Enter your username: ");
+      const password = await promptly.password("Enter your password: ", {
+        replace: "*",
+      });
+      const accessToken = await getAccessToken(
+        qServer,
+        username.trim(),
+        password.trim()
+      );
+      config.set(`${environment}.accessToken`, accessToken);
+    }
+
+    const accessToken = config.get(`${environment}.accessToken`);
+    const qServer = config.get(`${environment}.qServer`);
+    const isAccessTokenValid = await checkValidityOfAccessToken(
+      qServer,
+      accessToken
+    );
+
+    // Get a new access token in case its not valid anymore
+    if (!isAccessTokenValid) {
+      const username = await promptly.prompt("Enter your username: ");
+      const password = await promptly.password("Enter your password: ", {
+        replace: "*",
+      });
+      const accessToken = await getAccessToken(
+        qServer,
+        username,
+        password.trim()
+      );
+      config.set(`${environment}.accessToken`, accessToken);
+    }
+  }
+
+  return config;
+}
+
+async function getAccessToken(qServer, username, password) {
+  const response = await fetch(`${qServer}authenticate`, {
     method: "POST",
     body: JSON.stringify({
       username: username,
@@ -19,71 +150,21 @@ async function getQServerAccessToken(QServerBaseUrl, username, password) {
   }
 }
 
-async function checkValidityOfAccessToken(QServerBaseUrl, QServerAccessToken) {
-  const response = await fetch(`${QServerBaseUrl}user`, {
+async function checkValidityOfAccessToken(qServer, accessToken) {
+  const response = await fetch(`${qServer}user`, {
     headers: {
-      Authorization: `Bearer ${QServerAccessToken}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   });
   return response.ok;
 }
 
-async function getItem(QServerBaseUrl, QServerAccessToken, id) {
-  const response = await fetch(`${QServerBaseUrl}item/${id}`, {
-    headers: {
-      Authorization: `Bearer ${QServerAccessToken}`,
-    },
-  });
-  if (response.ok) {
-    return await response.json();
-  } else {
-    logUnexpectedError();
-  }
-}
-
-async function saveItem(QServerBaseUrl, QServerAccessToken, item) {
-  delete item.updatedDate;
-  const response = await fetch(`${QServerBaseUrl}item`, {
-    method: "PUT",
-    body: JSON.stringify(item),
-    headers: {
-      Authorization: `Bearer ${QServerAccessToken}`,
-      "Content-Type": "application/json",
-    },
-  });
-  if (response.ok) {
-    return await response.json();
-  } else {
-    logUnexpectedError();
-  }
-}
-
-function logUnexpectedError() {
-  console.log(
-    "An unexpected error occured. Please check the entered information and try again."
-  );
-}
-
-function getCurrentDirectoryBase() {
-  return path.basename(process.cwd());
-}
-
-function validateConfig(config) {
-  const isValid = ajv.validate(require("./schema.json"), config);
-  if (!isValid) {
-    console.log(ajv.errorsText());
-    process.exit(1);
-  }
-
-  return isValid;
-}
-
 module.exports = {
+  updateItem: updateItem,
   getItem: getItem,
   saveItem: saveItem,
-  getQServerAccessToken: getQServerAccessToken,
-  checkValidityOfAccessToken: checkValidityOfAccessToken,
   getCurrentDirectoryBase: getCurrentDirectoryBase,
+  setupConfig: setupConfig,
   validateConfig: validateConfig,
   logUnexpectedError: logUnexpectedError,
 };
--- bin/commands/updateItem/updateItem.js
+++ bin/commands/updateItem/updateItem.js
@@ -1,81 +1,20 @@
-const promptly = require("promptly");
-const Configstore = require("configstore");
-const deepmerge = require("deepmerge");
-const package = require("../../../package.json");
-const config = new Configstore(package.name, {});
 const helpers = require("./helpers.js");
 const fs = require("fs");
 
-async function updateItem(QServerBaseUrl, QServerAccessToken, item) {
-  const existingItem = await helpers.getItem(
-    QServerBaseUrl,
-    QServerAccessToken,
-    item.metadata.id
-  );
-  const newItem = deepmerge(existingItem, item.item, {
-    arrayMerge: (destArr, srcArr) => srcArr,
-  });
-
-  await helpers.saveItem(QServerBaseUrl, QServerAccessToken, newItem);
-  console.log(`Successfully updated item with id ${item.metadata.id}`);
-}
-
 module.exports = async function (command) {
   try {
-    if (command.clear) {
-      config.clear();
-    }
-
-    if (!config.get("QServerBaseUrl")) {
-      const QServerBaseUrl = new URL(
-        await promptly.prompt("Enter the Q-Server url: ")
-      ).toString();
-      config.set("QServerBaseUrl", QServerBaseUrl);
-    }
-
-    if (!config.get("QServerAccessToken")) {
-      const QServerBaseUrl = config.get("QServerBaseUrl");
-      const username = await promptly.prompt("Enter your username: ");
-      const password = await promptly.password("Enter your password: ", {
-        replace: "*",
-      });
-      const QServerAccessToken = await helpers.getQServerAccessToken(
-        QServerBaseUrl,
-        username.trim(),
-        password.trim()
-      );
-      config.set("QServerAccessToken", QServerAccessToken);
-    }
-
-    const QServerAccessToken = config.get("QServerAccessToken");
-    const QServerBaseUrl = config.get("QServerBaseUrl");
-    const isAccessTokenValid = await helpers.checkValidityOfAccessToken(
-      QServerBaseUrl,
-      QServerAccessToken
-    );
-
-    // Get a new access token in case its not valid anymore
-    if (!isAccessTokenValid) {
-      const username = await promptly.prompt("Enter your username: ");
-      const password = await promptly.password("Enter your password: ", {
-        replace: "*",
-      });
-      const QServerAccessToken = await helpers.getQServerAccessToken(
-        QServerBaseUrl,
-        username,
-        password.trim()
-      );
-      config.set("QServerAccessToken", QServerAccessToken);
-    }
-
     const QConfigPath = `${process.cwd()}/q.config.json`;
     if (fs.existsSync(QConfigPath)) {
       const qConfig = JSON.parse(fs.readFileSync(QConfigPath));
-      const isValidConfig = helpers.validateConfig(qConfig);
-      if (isValidConfig) {
+      const result = helpers.validateConfig(qConfig);
+      if (result.isValid) {
+        const config = await helpers.setupConfig(qConfig, command.clear);
         for (const item of qConfig.items) {
-          await updateItem(QServerBaseUrl, QServerAccessToken, item);
+          await helpers.updateItem(item, config);
         }
+      } else {
+        console.log(result.errorsText);
+        process.exit(1);
       }
     } else {
       console.log(
