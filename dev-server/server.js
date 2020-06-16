520257e4ade969474c922d273d8bfdc8132acf27
Manuel Roth <manuelroth@hotmail.ch>
2020-06-16 16:08:36 +0200

    Update dependencies

--- dev-server/server.js
+++ dev-server/server.js
@@ -1,13 +1,15 @@
 const Hapi = require("@hapi/hapi");
+const Joi = require("@hapi/joi");
 const Path = require("path");
 
 const server = new Hapi.Server({
   port: process.env.PORT || 5000,
   routes: {
     files: {
-      relativeTo: Path.join(__dirname, "public")
-    }
-  }
+      relativeTo: Path.join(__dirname, "public"),
+    },
+  },
 });
+server.validator(Joi);
 
 module.exports = server;
