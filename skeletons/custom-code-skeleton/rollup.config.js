3ee0416a9c339edce66705f676c90a2a4dc82e13
Manuel Roth <manuelroth@hotmail.ch>
2020-06-24 19:04:12 +0200

    Update to rollup v2

--- skeletons/custom-code-skeleton/rollup.config.js
+++ skeletons/custom-code-skeleton/rollup.config.js
@@ -1,12 +1,12 @@
-import svelte from "rollup-plugin-svelte";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
-import buble from "rollup-plugin-buble";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import buble from "@rollup/plugin-buble";
 import json from "@rollup/plugin-json";
 import svg from "rollup-plugin-svg";
 import html from "@rollup/plugin-html";
+import svelte from "rollup-plugin-svelte";
+import { terser } from "rollup-plugin-terser";
+import livereload from "rollup-plugin-livereload";
 import packageConfig from "./package.json";
 import qConfig from "./q.config.json";
 
