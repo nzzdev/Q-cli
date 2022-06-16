e73264ffecef3e3c09b1b7b23a250f74cb54de15
Michel Grautstueck <michel.grautstueck@nzz.ch>
2022-06-16 15:18:01 +0200

    Update et-utils-package-skeleton

--- skeletons/et-utils-package-skeleton/test/Service.spec.ts
+++ skeletons/et-utils-package-skeleton/test/Service.spec.ts
@@ -1,19 +1,10 @@
-import * as Lab from "@hapi/lab";
-import { expect } from "@hapi/code";
 import { someFunction } from "../src";
 
-// Hapi Lab init code
-const lab = Lab.script();
-const { describe, it, before } = lab;
-export { lab };
-
 describe("Service", () => {
-  before(() => {});
-
   it("should return the konami cheatcode", () => {
     const result = someFunction();
     const expectedResult = "Up, Up, Down, Down, Left, Right, Left, Right, B, A";
 
-    expect(result).to.equal(expectedResult);
+    expect(result).toEqual(expectedResult);
   });
 });
